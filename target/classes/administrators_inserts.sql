66ed343a653bca31a501f334af6e5bc986de01b5
Daniel Small <danjsmall16@gmail.com>
2026-04-19 21:52:13 +0100

    Removed taget

--- target/classes/administrators_inserts.sql
+++
@@ -1,6 +0,0 @@
-BEGIN TRANSACTION;
-
-INSERT INTO administrators (email, name) VALUES ('admin@healthassistant.com', 'System Admin');
-INSERT INTO administrators (email, name) VALUES ('root@healthassistant.com', 'Root');
-
-COMMIT;
